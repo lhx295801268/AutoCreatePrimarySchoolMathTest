685fcda1bda1afb746c882f8afc8f59fd0f55b49
TsunamiLee <769681799@qq.com>
2024-05-14 16:47:12 +0800

    add：爬虫测试

---
+++ requestQuestionTest.py
@@ -0,0 +1,15 @@
+﻿import requests
+from bs4 import BeautifulSoup
+
+import WriteFile
+# 奥数库基础网址
+rootUrl = "https://www.aoshuku.com/"
+
+requestObj = requests.get(rootUrl)
+
+soup = BeautifulSoup(requestObj.content)
+print(soup.prettify)
+
+netContent = soup.decode()
+
+WriteFile.writeContent2File(netContent, "./", "aushuku", "json")
